e28ff59fe14d75d9f150b4a1f6f96d9e2b129ba9
dragonee2000 <tsieneetan@gmail.com>
2022-01-26 00:17:28 -0600

    login, signup done, working on chat styling

--- client/src/Signup.js
+++ client/src/Signup.js
@@ -9,10 +9,105 @@ import {
   FormControl,
   TextField,
   FormHelperText,
+  makeStyles,
 } from "@material-ui/core";
 import { register } from "./store/utils/thunkCreators";
+import SmsIcon from "@material-ui/icons/Sms";
+import Background from "./assets/images/background.png";
 
-const Login = (props) => {
+const useStyles = makeStyles({
+  backgroundTint: {
+    position: "absolute",
+    height: "100vh",
+    width: "calc(100vw * 5 /12)",
+    background: "linear-gradient(#3A8DFF, #86B9FF);",
+    zIndex: "100",
+    opacity: 0.8532,
+  },
+  backgroundImg: {
+    height: "100vh",
+    width: "calc(100vw * 5 /12)",
+    backgroundImage: `url(${Background})`,
+    backgroundSize: "cover",
+  },
+  backgroundTextContainer: {
+    position: "absolute",
+    height: "100vh",
+    width: "calc(100vw * 5 /12)",
+    zIndex: "200",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    color: "white",
+  },
+  chatBubble: {
+    fontSize: 50,
+  },
+  backgroundText: {
+    fontSize: "26px",
+    fontFamily: "'Open Sans', sans-serif",
+  },
+  contents: {
+    height: "100vh",
+  },
+  createContainer: {
+    height: "5vh",
+    padding: "30px 42px 0 0",
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "flex-end",
+    alignItems: "center",
+  },
+  createHelperText: {
+    color: "#B0B0B0",
+    fontSize: "14px",
+    fontFamily: "'Open Sans'",
+    marginRight: "25px",
+  },
+  authContainer: {
+    height: "95vh",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "column",
+  },
+  authContainerAlignment: {
+    width: "60%",
+  },
+  authTitle: {
+    fontFamily: "Open Sans",
+    fontSize: "26px",
+    fontStyle: "normal",
+    fontWeight: 600,
+    lineHeight: "40px",
+  },
+  input: {
+    marginBottom: "20px",
+  },
+  createButton: {
+    padding: "20px 60px",
+    boxShadow: "0px 2px 12px rgba(74, 106, 149, 0.2)",
+    borderRadius: "5px",
+  },
+  submitBtnContainer: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: "40px",
+  },
+  submitBtn: {
+    borderRadius: "3px",
+    padding: "20px 70px",
+    fontFamily: "Open Sans",
+    fontWeight: 700,
+    fontStyle: "normal",
+    fontSize: "16px",
+  },
+});
+
+const Signup = (props) => {
+  const classes = useStyles();
   const history = useHistory();
   const { user, register } = props;
   const [formErrorMessage, setFormErrorMessage] = useState({});
@@ -37,72 +132,120 @@ const Login = (props) => {
   }
 
   return (
-    <Grid container justify="center">
-      <Box>
-        <Grid container item>
-          <Typography>Need to log in?</Typography>
-          <Button onClick={() => history.push("/login")}>Login</Button>
+    <Grid container justify="center" direction="row">
+      <Grid item md={5}>
+        <section className={classes.backgroundImg}>
+          <div className={classes.backgroundTint} />
+          <div className={classes.backgroundTextContainer}>
+            <SmsIcon className={classes.chatBubble} />
+            {/* <img src={ChatBubble} alt="chat-bubble" className={classes.chatBubble} /> */}
+            <Typography className={classes.backgroundText}>
+              Converse with anyone with any language
+            </Typography>
+          </div>
+        </section>
+      </Grid>
+      <Grid item md={7} className={classes.contents}>
+        <Grid container item className={classes.createContainer}>
+          <Typography className={classes.createHelperText}>
+            Already have an account?
+          </Typography>
+          <Button
+          color="primary"
+            className={classes.createButton}
+            onClick={() => history.push("/login")}
+          >
+            Login
+          </Button>
         </Grid>
-        <form onSubmit={handleRegister}>
-          <Grid>
+        <Grid className={classes.authContainer}>
+          <Grid className={classes.authContainerAlignment}>
             <Grid>
-              <FormControl>
-                <TextField
-                  aria-label="username"
-                  label="Username"
-                  name="username"
-                  type="text"
-                  required
-                />
-              </FormControl>
+              <Typography className={classes.authTitle}>
+                Create an account.
+              </Typography>
             </Grid>
-            <Grid>
-              <FormControl>
-                <TextField
-                  label="E-mail address"
-                  aria-label="e-mail address"
-                  type="email"
-                  name="email"
-                  required
-                />
-              </FormControl>
-            </Grid>
-            <Grid>
-              <FormControl error={!!formErrorMessage.confirmPassword}>
-                <TextField
-                  aria-label="password"
-                  label="Password"
-                  type="password"
-                  inputProps={{ minLength: 6 }}
-                  name="password"
-                  required
-                />
-                <FormHelperText>
-                  {formErrorMessage.confirmPassword}
-                </FormHelperText>
-              </FormControl>
-            </Grid>
-            <Grid>
-              <FormControl error={!!formErrorMessage.confirmPassword}>
-                <TextField
-                  label="Confirm Password"
-                  aria-label="confirm password"
-                  type="password"
-                  inputProps={{ minLength: 6 }}
-                  name="confirmPassword"
-                  required
-                />
-                <FormHelperText>
-                  {formErrorMessage.confirmPassword}
-                </FormHelperText>
-              </FormControl>
-            </Grid>
-            <Button type="submit" variant="contained" size="large">
-              Create
-            </Button>
+            <form onSubmit={handleRegister}>
+              <Grid>
+                <Grid>
+                  <FormControl margin="normal" required fullWidth>
+                    <TextField
+                      className={classes.input}
+                      aria-label="username"
+                      label="Username"
+                      name="username"
+                      type="text"
+                      required
+                    />
+                  </FormControl>
+                </Grid>
+                <Grid>
+                  <FormControl margin="normal" fullWidth>
+                    <TextField
+                      className={classes.input}
+                      label="E-mail address"
+                      aria-label="e-mail address"
+                      type="email"
+                      name="email"
+                      required
+                    />
+                  </FormControl>
+                </Grid>
+                <Grid>
+                  <FormControl
+                    error={!!formErrorMessage.confirmPassword}
+                    margin="normal"
+                    fullWidth
+                  >
+                    <TextField
+                      className={classes.input}
+                      aria-label="password"
+                      label="Password"
+                      type="password"
+                      inputProps={{ minLength: 6 }}
+                      name="password"
+                      required
+                    />
+                    <FormHelperText>
+                      {formErrorMessage.confirmPassword}
+                    </FormHelperText>
+                  </FormControl>
+                </Grid>
+                <Grid>
+                  <FormControl
+                    error={!!formErrorMessage.confirmPassword}
+                    margin="normal"
+                    fullWidth
+                  >
+                    <TextField
+                      label="Confirm Password"
+                      aria-label="confirm password"
+                      type="password"
+                      inputProps={{ minLength: 6 }}
+                      name="confirmPassword"
+                      required
+                    />
+                    <FormHelperText>
+                      {formErrorMessage.confirmPassword}
+                    </FormHelperText>
+                  </FormControl>
+                </Grid>
+                <Grid className={classes.submitBtnContainer}>
+                  <Button
+                    color="primary"
+                    type="submit"
+                    variant="contained"
+                    size="large"
+                    className={classes.submitBtn}
+                  >
+                    Create
+                  </Button>
+                </Grid>
+              </Grid>
+            </form>
           </Grid>
-        </form>
-      </Box>
+        </Grid>
+      </Grid>
     </Grid>
   );
 };
@@ -121,4 +264,4 @@ const mapDispatchToProps = (dispatch) => {
   };
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(Login);
+export default connect(mapStateToProps, mapDispatchToProps)(Signup);
--- client/src/components/Sidebar/CurrentUser.js
+++ client/src/components/Sidebar/CurrentUser.js
@@ -1,9 +1,11 @@
-import React from "react";
-import { Box, Typography } from "@material-ui/core";
+import React, { useState } from "react";
+import { Box, Typography, IconButton, Menu, MenuItem } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import { connect } from "react-redux";
 import { BadgeAvatar } from "./index";
 import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
+import { logout, fetchConversations } from "../../store/utils/thunkCreators";
+import { clearOnLogout } from "../../store";
 
 const useStyles = makeStyles(() => ({
   root: {
@@ -11,47 +13,84 @@ const useStyles = makeStyles(() => ({
     marginTop: 23,
     marginLeft: 6,
     display: "flex",
-    alignItems: "center"
+    alignItems: "center",
   },
   subContainer: {
     display: "flex",
     justifyContent: "space-between",
     alignItems: "center",
-    flexGrow: 1
+    flexGrow: 1,
   },
   username: {
     letterSpacing: -0.23,
     fontSize: 16,
     fontWeight: "bold",
-    marginLeft: 17
+    marginLeft: 17,
   },
   ellipsis: {
     color: "#95A7C4",
+    opacity: 0.5,
+  },
+  ellipsisBtn: {
     marginRight: 24,
-    opacity: 0.5
-  }
+  },
 }));
 
 const CurrentUser = (props) => {
   const classes = useStyles();
+  const { logout } = props;
+  const [anchorEl, setAnchorEl] = useState(null);
 
   const user = props.user || {};
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  }
+
+  const handleClose = () => {
+    setAnchorEl(null)
+  }
+
+  const handleLogout = async () => {
+    await logout(user.id);
+  }
 
   return (
     <Box className={classes.root}>
       <BadgeAvatar photoUrl={user.photoUrl} online={true} />
       <Box className={classes.subContainer}>
         <Typography className={classes.username}>{user.username}</Typography>
-        <MoreHorizIcon classes={{ root: classes.ellipsis }} />
+        <IconButton onClick={handleClick} classes={classes.ellipsisBtn}>
+          <MoreHorizIcon classes={{ root: classes.ellipsis }} />
+        </IconButton>
       </Box>
+      <Menu
+      id="user-menu"
+      anchorEl={anchorEl}
+      keepMounted
+      open={Boolean(anchorEl)}
+      onClose={handleClose}
+      >
+        <MenuItem onClick={handleLogout}>Logout</MenuItem>
+      </Menu>
     </Box>
   );
 };
 
 const mapStateToProps = (state) => {
   return {
-    user: state.user
+    user: state.user,
   };
 };
 
-export default connect(mapStateToProps)(CurrentUser);
+const mapDispatchToProps = (dispatch) => {
+  return {
+    logout: (id) => {
+      dispatch(logout(id));
+      dispatch(clearOnLogout());
+    }
+  };
+};
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(CurrentUser);
