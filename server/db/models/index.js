2406e0f999cba68ebad84b07a16c7be18094ac13
dragonee2000 <tsieneetan@gmail.com>
2022-02-08 01:19:08 -0600

    index file for the changes made

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,17 +1,30 @@
 const Conversation = require("./conversation");
 const User = require("./user");
 const Message = require("./message");
+const UserConversation = require("./userConversation");
+
 
 // associations
 
-User.hasMany(Conversation);
-Conversation.belongsTo(User, { as: "user1" });
-Conversation.belongsTo(User, { as: "user2" });
+User.hasMany(UserConversation);
+UserConversation.belongsTo(User);
+
+Conversation.hasMany(UserConversation);
+UserConversation.belongsTo(Conversation);
+
 Message.belongsTo(Conversation);
 Conversation.hasMany(Message);
+
+User.hasMany(Message);
+Message.belongsTo(User)
+// Conversation.belongsTo(User, { as: "user1" });
+// Conversation.belongsTo(User, { as: "user2" });
+// Message.belongsTo(Conversation);
+// Conversation.hasMany(Message);
 
 module.exports = {
   User,
   Conversation,
-  Message
+  Message,
+  UserConversation
 };
