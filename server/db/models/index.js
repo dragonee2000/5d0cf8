9d33a8b32d379995daf6e76d17b9859af6c0cda5
dragonee2000 <tsieneetan@gmail.com>
2022-02-10 15:44:53 -0600

    association

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -5,22 +5,14 @@ const UserConversation = require("./userConversation");
 
 
 // associations
-
-User.hasMany(UserConversation);
-UserConversation.belongsTo(User);
-
-Conversation.hasMany(UserConversation);
-UserConversation.belongsTo(Conversation);
+Conversation.belongsToMany(User, { through: 'UserConversation' });
+User.belongsToMany(Conversation, { through: "UserConversation "});
 
 Message.belongsTo(Conversation);
 Conversation.hasMany(Message);
 
 User.hasMany(Message);
 Message.belongsTo(User)
-// Conversation.belongsTo(User, { as: "user1" });
-// Conversation.belongsTo(User, { as: "user2" });
-// Message.belongsTo(Conversation);
-// Conversation.hasMany(Message);
 
 module.exports = {
   User,
